3551d99489a85a0316b20632f6b591350b825bc2
MAN98 <allanbernardo98@outlook.com>
2024-03-26 18:06:00 +0000

    Added Backup Storage private module

--- main.tf
+++ main.tf
@@ -204,6 +204,6 @@ resource "null_resource" "configure-cat-app" {
 
 module "backupstorage" {
   source = "app.terraform.io/98Solutions/azure-backup/azurerm"
-  version = "1.0.2"
+  version = "1.0.3"
   resource_group_name = "${var.prefix}-private"
 }
